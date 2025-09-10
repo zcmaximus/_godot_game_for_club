9bf61e474c9ff96b9b898f6091bba747eef0ed16
zcmaximus <zcmaximus8.10@gmail.com>
2025-09-10 18:16:14 +0200

    .

--- scripts/player_code.gd
+++ scripts/player_code.gd
@@ -6,4 +6,6 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * SPEED
+	
+	print(direction)
 	move_and_slide()
