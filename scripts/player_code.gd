e3d024c9a1b5d493d0e1bedcd9ee66605b10888e
zcmaximus <zcmaximus8.10@gmail.com>
2025-09-12 19:52:16 +0200

    movement scrpit

--- scripts/player_code.gd
+++ scripts/player_code.gd
@@ -1,11 +1,29 @@
 extends CharacterBody2D
 
-
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var SPEED = 300.0
 
+var last_direction: Vector2 = Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * SPEED
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	
-	print(direction)
+	animated_sprite_2d.flip_h = false
+	
+	if direction == Vector2.ZERO:
+		animated_sprite_2d.play("idle")
+		print("idle")
+	else:
+		if direction.x > 0:
+			animated_sprite_2d.flip_h = false
+			print("right")
+		elif direction.x < 0:
+			animated_sprite_2d.flip_h = true
+			print("left")
+			
+		animated_sprite_2d.play("running")
+		print("running")
+		
+	velocity = direction * SPEED
 	move_and_slide()
+	
